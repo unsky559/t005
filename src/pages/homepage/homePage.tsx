931c811c377f14e4e8cb3eaf51c59c3e817c050a
unsky559 <6kostya@gmail.com>
2022-07-28 16:53:41 +0300

    feat: home page

---
+++ src/pages/homepage/homePage.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Heading from '../../componetns/heading/heading';
+import Text from '../../componetns/text/text';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Heading>Hello</Heading>
+      <Text>Hello</Text>
+    </div>
+  );
+};
+
+export default HomePage;
