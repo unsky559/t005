978f230c9933d1be486636be6ad233adbcafebe5
unsky559 <6kostya@gmail.com>
2022-07-30 01:31:06 +0300

    add: Tooltips to card

--- src/componetns/card/card.tsx
+++ src/componetns/card/card.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import styles from './card.module.scss';
+import Tooltip from '../tooltip/tooltip';
 
 type propType = {
   title: string,
@@ -11,10 +12,18 @@ const Card = (props: propType) => {
   return (
     <article className={styles.card}>
       <img className={styles.image} src={props.imageURL} alt=""/>
-      <p className={styles.title}>{props.title}</p>
-      <p className={styles.description}>{props.description[0]}</p>
-      <p className={styles.description}>{props.description[1]}</p>
-      <p className={styles.description}>{props.description[2]}</p>
+      <Tooltip text={props.title}>
+        <p className={styles.title}>{props.title}</p>
+      </Tooltip>
+      <Tooltip text={props.description[0]}>
+        <p className={styles.description}>{props.description[0]}</p>
+      </Tooltip>
+      <Tooltip text={props.description[1]}>
+        <p className={styles.description}>{props.description[1]}</p>
+      </Tooltip>
+      <Tooltip text={props.description[2]}>
+        <p className={styles.description}>{props.description[2]}</p>
+      </Tooltip>
     </article>
   );
 };
